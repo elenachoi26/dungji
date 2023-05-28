d2ee263c230a1fc9e6928f0f3dfb96b2a2374869
haeun1 <87661039+haeun1@users.noreply.github.com>
2023-05-28 22:49:19 +0900

    calendar commit

---
+++ app/src/main/java/com/example/dungziproject/navigation/CalendarFragment.kt
@@ -0,0 +1,36 @@
+package com.example.dungziproject.navigation
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.dungziproject.CalendarMainActivity
+import com.example.dungziproject.R
+import com.example.dungziproject.databinding.FragmentCalendarBinding
+import com.example.dungziproject.databinding.FragmentHomeBinding
+
+class CalendarFragment :Fragment() {
+    var binding: FragmentCalendarBinding?=null
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCalendarBinding.inflate(inflater, container, false)
+
+        //일주일 시간표
+
+
+        //월 캘린더
+        val intent = Intent(activity, CalendarMainActivity::class.java)
+        binding!!.monthButton.setOnClickListener {
+            startActivity(intent)
+        }
+
+        return binding!!.root
+    }
+
+
+}
